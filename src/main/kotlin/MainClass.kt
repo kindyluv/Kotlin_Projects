6bbcf4c0d1bd2bf6ae0337cced3c93f58569adf3
kindyluv <onyeukwuamara@gmail.com>
2022-01-31 16:01:32 +0100

    ..

---
+++ src/main/kotlin/MainClass.kt
@@ -0,0 +1,11 @@
+
+fun main(args: Array<String>){
+     val a: Int=1; val b: Boolean= true; var c: Int=1;
+     var result: Int; var booleanResult: Boolean;
+    result = -a;
+    println("-a = $result")
+    booleanResult = !b
+    println("!b = $booleanResult")
+    println("--c = ${--c}")
+
+}
