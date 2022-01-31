0edeceaa530ae303bc17ec7c97cfb48d1ad1d301
kindyluv <onyeukwuamara@gmail.com>
2022-01-31 18:07:59 +0100

    ..

---
+++ src/main/kotlin/Input.kt
@@ -0,0 +1,12 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+    print("Enter text: ")
+    val stringInput = readLine()!!
+    println("You entered: $stringInput")
+
+    val read = Scanner(System.`in`)
+    print("Enter any number: ")
+    var numb1 = read.nextInt()
+
+}
