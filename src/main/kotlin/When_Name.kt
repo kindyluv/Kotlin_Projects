c05268674d842e3d0d2930a1fdd9ceff00c60343
kindyluv <onyeukwuamara@gmail.com>
2022-02-01 10:29:26 +0100

    ..

---
+++ src/main/kotlin/When_Name.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    var name:String = "Lois"
+    when (name) {
+        "Lois" -> println("Correct username $name")
+        else -> println("Invalid username $name")
+    }
+}
