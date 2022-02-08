2961bac4335fd92189508a323936372227e034b4
kindyluv <onyeukwuamara@gmail.com>
2022-02-08 09:32:59 +0100

    ...

---
+++ src/main/kotlin/assignment/Question12CharOrNot.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    print("Enter an alphapet: ")
+    var character = readLine()!!
+    println()
+
+    if(character >='a' && character <= 'z' || character >= 'A' && character <= 'Z') {
+        println("$character is an alphabet")
+    }else{
+        println("$character is not an alphabet")
+    }
+}
