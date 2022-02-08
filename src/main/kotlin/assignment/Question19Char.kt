a48c0a1efdbd390ff83ff3eb82ce138f6fac9b3b
kindyluv <onyeukwuamara@gmail.com>
2022-02-08 09:33:41 +0100

    ...

---
+++ src/main/kotlin/assignment/Question19Char.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+
+//    print("Enter words: ")
+//    val words = readLine()!!.toChar()
+//
+//    words.isUpperCase()
+    for (word in 65..90){
+        println(word.toChar())
+    }
+}
