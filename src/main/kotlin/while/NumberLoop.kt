f9f04a61b9e4bc24f1ed7ae67be24fb86c125305
kindyluv <onyeukwuamara@gmail.com>
2022-02-08 09:32:50 +0100

    ...

--- src/main/kotlin/while/NumberLoop.kt
+++ src/main/kotlin/while/NumberLoop.kt
@@ -1,2 +1,14 @@
 package `while`
 
+fun main(args: Array<String>){
+    var num: Int = 10;
+    var sum: Int = 0;
+    while(num>=5){
+        println("Loop: $num")
+        if(num % 2==0){
+            sum +=num
+        }
+        num--
+    }
+    println("Sum: $sum")
+}
