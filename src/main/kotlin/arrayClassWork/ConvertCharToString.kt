3146252e6613849c0c34ed116463384ec88e3ec1
kindyluv <onyeukwuamara@gmail.com>
2022-02-14 15:40:33 +0100

    ..

---
+++ src/main/kotlin/arrayClassWork/ConvertCharToString.kt
@@ -0,0 +1,11 @@
+package arrayClassWork
+
+fun main(args: Array<String>) {
+    val word: Char = 'p'
+
+    println(charToString(word))
+}
+
+fun charToString(char: Char): String {
+    return char.toString()
+}
