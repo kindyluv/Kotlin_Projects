b3813ec566cebb49f61fd940ec48cc62b03dd9bd
kindyluv <onyeukwuamara@gmail.com>
2022-02-14 15:42:10 +0100

    ..

---
+++ src/main/kotlin/forLoop/Loop.kt
@@ -0,0 +1,28 @@
+fun main(args: Array<String>) {
+    print("for(i in 1..5) print(i) = ")
+    for(i in 1..5) print(i)
+    println()
+    println()
+
+    print("for(i in 5..1) print(i) = ")
+    for(i in 5..1) print(i)
+    println()
+    println()
+
+    print("for (i in 5 downTo 1) print(i) = ")
+    for (i in 5 downTo 1) print(i)
+    println()
+    println()
+
+    print("for (i in 1..5 step 2) print(i) = ")
+    for (i in 1..5 step 2) print(i)
+    println()
+    println()
+
+    print("for (i in 5 downTo 1 step 2) print(i) = ")
+    for (i in 5 downTo 1 step 2) print(i)
+    println()
+    println()
+
+
+}
