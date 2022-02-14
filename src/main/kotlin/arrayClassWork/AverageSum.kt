76f0298c583e94d0563be24c3f53b3a1653653ac
kindyluv <onyeukwuamara@gmail.com>
2022-02-14 15:39:50 +0100

    ..

---
+++ src/main/kotlin/arrayClassWork/AverageSum.kt
@@ -0,0 +1,21 @@
+package arrayClassWork
+
+fun main(args: Array<String>) {
+
+    val numbers = arrayOf<Int>(1,4,42,5,10,9,6)
+
+    average(numbers)
+}
+
+fun average(array: Array<Int>){
+    var sum: Int = 0
+    var average: Double = 0.0
+
+    for (i in array){
+        sum = sum + i
+    }
+
+    average = (sum / 2).toDouble()
+    println("Average sum of $sum = $average")
+}
+
