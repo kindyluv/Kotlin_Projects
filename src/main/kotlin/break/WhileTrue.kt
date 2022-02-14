bd0658f65f109999122d633719058454cf793b0f
kindyluv <onyeukwuamara@gmail.com>
2022-02-14 15:44:07 +0100

    ..

---
+++ src/main/kotlin/break/WhileTrue.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+    var sum = 0
+    var number: Int
+    while (true){
+        print("Enter a number: ")
+        number = readLine()!!.toInt()
+
+        if (number == 0) break;
+        sum += number;
+    }
+        println(sum)
+
+}
