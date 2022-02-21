24cab4a8def9b88d52e5a5ad8d5e6a2bdbd40e3c
kindyluv <onyeukwuamara@gmail.com>
2022-02-21 19:51:43 +0100

    ..

--- src/main/kotlin/arrayClassWork/SumOfNaturalNumberInRecursion.kt
+++ src/main/kotlin/arrayClassWork/SumOfNaturalNumberInRecursion.kt
@@ -2,15 +2,14 @@ package arrayClassWork
 
 fun main(args: Array<String>) {
     val numb = 100;
-    val result = naturalNumRecursion(numb);
-
-    println(result);
+    val result = naturalNumRecursion(numb)
+    println("$result ")
 }
-tailrec fun naturalNumRecursion(n: Int, result: Int = 0): Int{
-    return if (n <= 1){
-        result;
-    }else{
-        naturalNumRecursion((n-1), n+result);
+
+tailrec fun naturalNumRecursion(n: Int, result: Int = 0): Int {
+   return when(n <= 1){
+        true -> return result
+        else -> naturalNumRecursion((n-1), n+result)
     }
 
 }
