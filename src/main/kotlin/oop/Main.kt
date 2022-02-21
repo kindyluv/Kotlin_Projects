f6cff2b2d2dd134c8eb8fc8eb61a39518e3252ca
kindyluv <onyeukwuamara@gmail.com>
2022-02-21 19:51:27 +0100

    ..

---
+++ src/main/kotlin/oop/Main.kt
@@ -0,0 +1,10 @@
+package oop
+
+fun main(args: Array<String>) {
+    val name = FirstClass()
+
+    println(name.ageInDays())
+
+    val person = Person("Lois", "Precious", 200)
+    val auto1 = Automobile("GLK", 2021)
+}
