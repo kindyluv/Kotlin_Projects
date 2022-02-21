9835d9517b5ce5a86c17437b7b05744f4b0f7f7d
kindyluv <onyeukwuamara@gmail.com>
2022-02-21 19:51:16 +0100

    ..

--- src/main/kotlin/arrayClassWork/LargestSumAmongThree.kt
+++ src/main/kotlin/arrayClassWork/LargestSumAmongThree.kt
@@ -1,16 +1,21 @@
 package arrayClassWork
 
 fun main(args: Array<String>) {
-    val jl: Int = 29
+    val jl: Int = 2
     val kl: Int = 11
     val ll: Int = 4
 
-    val result = largestSum(jl, kl, ll)
-    println("$result is the largest number among three numbers")
+    when(jl >= kl && jl >= ll) {
+         true -> println("$jl is the largest number.")
+        false -> println("$kl is the largest number.")
+        else -> println("$ll is the largest number.")
+    }
+//    val result = largestSum(jl, kl, ll)
+//    println("$result is the largest number among three numbers")
 }
-fun largestSum(j: Int, k: Int, l: Int): Int{
-    if (j > k && j > l) return j
-    else if (k > j && k > l) return k
-    else if (l > j && l > k) return l
-    return 0
-}
+//fun largestSum(j: Int, k: Int, l: Int): Int{
+//    if (j > k && j > l) return j
+//    else if (k > j && k > l) return k
+//    else if (l > j && l > k) return l
+//    return 0
+//}
